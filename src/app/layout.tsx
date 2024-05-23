8d71a02b43248ab33ee639d8247c28dec25e7823
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-23 15:11:58 -0300

    feature: implement font IBMPlex Sans this project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable camelcase */
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { IBM_Plex_Sans } from 'next/font/google'
+import '../styles/globals.css'
 import { Header } from '@/components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const ibm_plex_sans = IBM_Plex_Sans({ weight: ['700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={ibm_plex_sans.className}>
         <Header />
         {children}
       </body>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css'
+
 export const Header = () => {
-  return <header>SEVN NEWS</header>
+  return <header className={styles.header}>SEVN NEWS</header>
 }
