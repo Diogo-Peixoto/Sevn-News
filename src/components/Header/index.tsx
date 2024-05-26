9c59fa9c21213e139d2bf1f8533f3b79d4c40b56
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-26 11:12:58 -0300

    feature: Adding a button to return to the home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,29 @@
+'use client'
+
+import Image from 'next/image'
 import styles from './styles.module.css'
+import { usePathname, useRouter } from 'next/navigation'
 
 export const Header = () => {
-  return <header className={styles.header}>SEVN NEWS</header>
+  const router = useRouter()
+  const isPreviousPage = usePathname().includes('article')
+
+  return (
+    <header className={styles.header}>
+      {isPreviousPage && (
+        <button
+          className={styles.buttonPreviousPage}
+          onClick={() => router.back()}
+        >
+          <Image
+            src="/header/arrow-left.svg"
+            alt="Return to the previous page"
+            width={24}
+            height={24}
+          />
+        </button>
+      )}
+      SEVN NEWS
+    </header>
+  )
 }
